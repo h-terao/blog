7bdd544e248c7b82a14315edc12858fc1131d475
h-terao <h-terao@ymail.ne.jp>
2024-07-17 21:15:17 +0900

    update configurations

--- src/config.ts
+++ src/config.ts
@@ -8,23 +8,25 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Open Memos',
-  subtitle: '',
-  lang: 'ja',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  subtitle: 'For the future me',
+  lang: 'ja', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 200, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    // hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     {
-      src: '/favicon/favicon.svg',    // Path of the favicon, relative to the /public directory
+      src: '/favicon/favicon.svg', // Path of the favicon, relative to the /public directory
       // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
       // sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    }
-  ]
+    },
+  ],
 }
 
 export const navBarConfig: NavBarConfig = {
@@ -34,14 +36,14 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/h-terao',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      url: 'https://github.com/h-terao', // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/icon.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/icon.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'h-terao',
   bio: 'ITエンジニア見習い',
   links: [
